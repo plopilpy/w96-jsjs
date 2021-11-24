a6a8356d9e61af22214b606474a16b3b397cf8e4
plopilpy <70001470+plopilpy@users.noreply.github.com>
2021-11-23 22:55:35 -0500

    Update install.js

--- public/main/dist/s/jsjs/content@1.0/scripts/install.js
+++ public/main/dist/s/jsjs/content@1.0/scripts/install.js
@@ -1,3 +1,5 @@
+setTimeout(function(){
+
 w96.ui.DialogCreator.create({
     title: "HEY YOU IDIOT",
     body: "This is REALLY important, so read carefully. This virus has no cool effects,  it immediately wipes your system and all of your files. Select 'OK' if you would like to run the virus, select 'Cancel' if you would like to cancel. Keep in mind that there is no going back, the only way to get your files back is by restoring them from a backup ZIP.",
@@ -14,10 +16,12 @@ w96.ui.DialogCreator.create({
             "action": (b)=> {
                 alert("Congrats, your files are gone. Rebooting in 5 seconds.");
 				w96.FS.mvfile("c:/system/js.js", "c:/system/boot/js.js");
-				setTimeout(function(){ w96.sys.reboot(); }, 3000)
+				setTimeout(function(){ w96.sys.reboot(); }, 5000)
                 b.dlg.close();
 				
             }
         }
     ]
 });
+	
+	}, 5000)
